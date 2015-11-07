5e6a6815757f43cc209263c47bb3ce4622bd40d4
juanchel <juanchen.li@redbeacon.com>
2015-11-07 13:46:19 -0800

    Add working routes for two different players

--- meteor-ttt.js
+++ meteor-ttt.js
@@ -1,4 +1,6 @@
-Grid = new Mongo.Collection('grid');
+'use strict';
+
+var Grid = new Mongo.Collection('grid');
 
 if (Meteor.isClient) {
   Template.board.helpers({
@@ -8,8 +10,19 @@ if (Meteor.isClient) {
   });
 
   Template.row.events({
-    'click .ttt-button': function(e) {
-      alert('ayy');
+    'click .ttt-button.player1': function(e) {
+      takeMove(e, 1);
+    },
+    'click .ttt-button.player2': function(e) {
+      takeMove(e, 2);
+    }
+  });
+
+  Template.registerHelper('player', function() {
+    if (Router.current().route.getName() == 'player1') {
+      return '1';
+    } else {
+      return '2';
     }
   });
 }
@@ -19,23 +32,56 @@ Router.route('/', function() {
 });
 
 Router.route('/player1', function () {
-  this.render('one', {
-    data: {
-      player: '1'
-    }
-  });
+  this.render('play');
 });
 
 Router.route('/player2', function () {
-  this.render('two', {
-    data: {
-      player: '2'
-    }
-  });
+  this.render('play');
 });
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Grid.remove({});
+    Grid.insert({vals: ['X', ' ', ' ']});
+    Grid.insert({vals: [' ', ' ', ' ']});
+    Grid.insert({vals: [' ', ' ', ' ']});
   });
+}
+
+/**
+ * Gets the current grid of the game
+ * @returns {Array} Array of nine characters representing the game board
+ */
+function getGrid() {
+  var t = Grid.find({}).fetch()[0]['vals'];
+  t.push.apply(t, Grid.find({}).fetch()[1]['vals']);
+  t.push.apply(t, Grid.find({}).fetch()[2]['vals']);
+  return t;
+}
+
+/**
+ * Allows a player to take a move
+ * @param {Object} e event passed in by the template
+ * @param {number} player the player number
+ */
+function takeMove(e, player) {
+
+  // Use the event to find the column and row of the clicked element
+  var $col = $(e.currentTarget);
+  var colNum;
+  var rowNum = $(e.currentTarget).parent().data('row');
+
+  if ($col.hasClass('col1')) {
+    colNum = 1;
+  } else if ($col.hasClass('col2')) {
+    colNum = 2;
+  } else {
+    colNum = 0;
+  }
+
+  // Update it in the collection
+  var rowId = Grid.find({}).fetch()[rowNum]['_id'];
+  var rowVals = Grid.find({}).fetch()[rowNum]['vals'];
+  rowVals[colNum] = player == 1 ? 'X' : 'O';
+  Grid.update(rowId, {$set: {vals: rowVals}});
 }

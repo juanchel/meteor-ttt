6dc2d70da93b06ff2d75a688f77b82d4739965f0
juanchel <juanchen.li@redbeacon.com>
2015-11-06 20:08:44 -0800

    Add iron router basic routes

--- meteor-ttt.js
+++ meteor-ttt.js
@@ -1,34 +1,20 @@
 Grid = new Mongo.Collection("grid");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-  Template.body.helpers({
+  Template.board.helpers({
     grid: function () {
       return Grid.find({});
-    },
-    rows: [
-      {vals: ["0", "3", "0"]},
-      {vals: ["0", "0", "0"]},
-      {vals: ["0", "1", "0"]}
-    ]
+    }
   });
-
 }
+
+Router.route('/', function() {
+  this.render('main');
+});
+
+Router.route('/player1');
+
+Router.route('/player2');
 
 if (Meteor.isServer) {
   Meteor.startup(function () {

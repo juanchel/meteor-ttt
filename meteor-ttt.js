6cd43ca961eb831bb8d8c0382700c366497ca28a
juanchel <juanchen.li@redbeacon.com>
2015-11-06 20:39:32 -0800

    Add basic event handling

--- meteor-ttt.js
+++ meteor-ttt.js
@@ -1,9 +1,15 @@
-Grid = new Mongo.Collection("grid");
+Grid = new Mongo.Collection('grid');
 
 if (Meteor.isClient) {
   Template.board.helpers({
     grid: function () {
       return Grid.find({});
+    }
+  });
+
+  Template.row.events({
+    'click .ttt-button': function(e) {
+      alert('ayy');
     }
   });
 }
@@ -12,9 +18,21 @@ Router.route('/', function() {
   this.render('main');
 });
 
-Router.route('/player1');
+Router.route('/player1', function () {
+  this.render('one', {
+    data: {
+      player: '1'
+    }
+  });
+});
 
-Router.route('/player2');
+Router.route('/player2', function () {
+  this.render('two', {
+    data: {
+      player: '2'
+    }
+  });
+});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
